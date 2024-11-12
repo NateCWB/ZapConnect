acd486929c2f0175c4cff2ec2a479076d83cd8b7
NateCWB <mattosnata99@gmail.com>
2024-11-11 22:53:14 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Formulário de Contratação
+document.querySelector('.contract-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulário enviado com sucesso!');
+});
