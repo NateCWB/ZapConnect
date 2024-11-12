6efc97e41d85a41814c4d36c4e5d6dcb07780e20
NateCWB <mattosnata99@gmail.com>
2024-11-11 23:03:25 -0300

    Started

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,21 @@ document.querySelector('.contract-form').addEventListener('submit', function(eve
     event.preventDefault();
     alert('Formulário enviado com sucesso!');
 });
+
+// Função para abrir o modal
+function openModal() {
+    document.getElementById('loginModal').style.display = 'block';
+}
+
+// Função para fechar o modal
+function closeModal() {
+    document.getElementById('loginModal').style.display = 'none';
+}
+
+// Fechar o modal ao clicar fora dele
+window.onclick = function(event) {
+    const modal = document.getElementById('loginModal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+};
