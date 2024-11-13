a4fa5900dfadd8be5e63fed3f114f3e00d284980
NateCWB <mattosnata99@gmail.com>
2024-11-12 21:53:40 -0300

    configurabn

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,24 @@ window.onclick = function(event) {
         modal.style.display = 'none';
     }
 };
+
+function sendEmail(event) {
+    event.preventDefault(); // Evita o recarregamento da página ao enviar o formulário
+
+    // Pega os dados do formulário
+    const formData = {
+        plan: document.getElementById("planSelect").value,
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value
+    };
+
+    // Envia o e-mail usando o serviço e o template do EmailJS
+    emailjs.send("service_2ds4ke9", "template_g7qbl5i", formData)
+        .then(() => {
+            alert("E-mail enviado com sucesso!");
+        })
+        .catch((error) => {
+            console.error("Erro ao enviar o e-mail:", error);
+            alert("Ocorreu um erro ao enviar o e-mail. Tente novamente.");
+        });
+}
